14d1bb17c80b9c766506f2c726f18c8088be0e67
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-22 11:37:42 +0900

    feat: Creating More Java Spring Beans in Spring Java Configuration File

--- 01. Spring/study-spring-framework/src/main/java/com/jikim/study/HelloWorldConfiguration.java
+++ 01. Spring/study-spring-framework/src/main/java/com/jikim/study/HelloWorldConfiguration.java
@@ -3,11 +3,34 @@ package com.jikim.study;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Eliminate verbosity in creating Java Beans
+// Public accessor methods, constructor, equals, hashcode
+// and toString are automatically created.
+// Released in JDK 16.
+record Person(String name, int age) { }
+
+record Address(String firstLine, String city) { }
+
 @Configuration
 public class HelloWorldConfiguration {
 
 	@Bean
 	public String name() {
 		return "Jihun";
+	}
+
+	@Bean
+	public int age() {
+		return 28;
+	}
+
+	@Bean
+	public Person person() {
+		return new Person("Kim", 20);
+	}
+
+	@Bean
+	public Address address() {
+		return new Address("Baker Street", "London");
 	}
 }
