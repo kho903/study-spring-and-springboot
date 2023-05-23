652603e200210892d37577c88949b5d34f009ed2
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-23 18:34:01 +0900

    feat: Primary and Qualifier Annotations for Spring Components

--- 01. Spring/study-spring-framework-02/src/main/java/com/jikim/study/game/MarioGame.java
+++ 01. Spring/study-spring-framework-02/src/main/java/com/jikim/study/game/MarioGame.java
@@ -1,5 +1,10 @@
 package com.jikim.study.game;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class MarioGame implements GamingConsole {
 
 	@Override
--- 01. Spring/study-spring-framework-02/src/main/java/com/jikim/study/game/SuperContraGame.java
+++ 01. Spring/study-spring-framework-02/src/main/java/com/jikim/study/game/SuperContraGame.java
@@ -1,5 +1,10 @@
 package com.jikim.study.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("SuperContraGameQualifier")
 public class SuperContraGame implements GamingConsole {
 
 	@Override
