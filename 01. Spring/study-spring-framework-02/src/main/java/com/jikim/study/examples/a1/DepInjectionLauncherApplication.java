c5a920f4db143443867060c999b7d8e49af6066b
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-23 18:48:50 +0900

    feat: Dependency Injection 1 - using @Autowired

---
+++ 01. Spring/study-spring-framework-02/src/main/java/com/jikim/study/examples/a1/DepInjectionLauncherApplication.java
@@ -0,0 +1,57 @@
+package com.jikim.study.examples.a1;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+// MyBusinessClass
+// Dependency1
+// Dependency2
+
+@Component
+class MyBusinessClass {
+	@Autowired
+	Dependency1 dependency1;
+
+	@Autowired
+	Dependency2 dependency2;
+
+	@Override
+	public String toString() {
+		return "MyBusinessClass{" +
+			"dependency1=" + dependency1 +
+			", dependency2=" + dependency2 +
+			'}';
+	}
+}
+
+@Component
+class Dependency1 {
+
+}
+
+@Component
+class Dependency2 {
+
+}
+
+@Configuration
+@ComponentScan
+public class DepInjectionLauncherApplication {
+
+	public static void main(String[] args) {
+		try (var context =
+				 new AnnotationConfigApplicationContext
+					 		(DepInjectionLauncherApplication.class)) {
+
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+
+			System.out.println(context.getBean(MyBusinessClass.class));
+		}
+	}
+}
