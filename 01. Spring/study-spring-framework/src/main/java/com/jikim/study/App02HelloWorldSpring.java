3450aefc18eacbd1ba8ecf53a174d1f97a843070
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-23 16:28:29 +0900

    feat: Implementing auto wiring in spring framework java configuration file

--- 01. Spring/study-spring-framework/src/main/java/com/jikim/study/App02HelloWorldSpring.java
+++ 01. Spring/study-spring-framework/src/main/java/com/jikim/study/App02HelloWorldSpring.java
@@ -15,6 +15,10 @@ public class App02HelloWorldSpring {
 		System.out.println(context.getBean("name"));
 		System.out.println(context.getBean("age"));
 		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("address"));
+		System.out.println(context.getBean("person2MethodCall"));
+		System.out.println(context.getBean("person3Parameters"));
+		System.out.println(context.getBean("customAddress"));
+
+		// System.out.println(context.getBean(Address.class));
 	}
 }
--- 01. Spring/study-spring-framework/src/main/java/com/jikim/study/HelloWorldConfiguration.java
+++ 01. Spring/study-spring-framework/src/main/java/com/jikim/study/HelloWorldConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 // Public accessor methods, constructor, equals, hashcode
 // and toString are automatically created.
 // Released in JDK 16.
-record Person(String name, int age) { }
+record Person(String name, int age, Address address) { }
 
 record Address(String firstLine, String city) { }
 
@@ -26,11 +26,26 @@ public class HelloWorldConfiguration {
 
 	@Bean
 	public Person person() {
-		return new Person("Kim", 20);
+		return new Person("Kim", 20, new Address("Main Street", "Utrecht"));
 	}
 
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address()); // name, age
+	}
+
+	@Bean
+	public Person person3Parameters(String name, int age, Address address2) { // name, age, customAddress
+		return new Person(name, age, address2); // name, age
+	}
+
+	@Bean(name = "customAddress")
 	public Address address() {
 		return new Address("Baker Street", "London");
+	}
+
+	@Bean(name = "address2")
+	public Address address2() {
+		return new Address("Street", "Suwon");
 	}
 }
