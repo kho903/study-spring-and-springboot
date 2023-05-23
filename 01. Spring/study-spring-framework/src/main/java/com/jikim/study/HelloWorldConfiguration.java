b699fada2e0bda36177006d8060f195d3bccb2e5
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-23 17:12:19 +0900

    feat: Bean Auto Wiring - Primary & Qualifier
    - @Primary, @Qualifier
    - context.getBeanDefinitionNames()

--- 01. Spring/study-spring-framework/src/main/java/com/jikim/study/HelloWorldConfiguration.java
+++ 01. Spring/study-spring-framework/src/main/java/com/jikim/study/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.jikim.study;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // Eliminate verbosity in creating Java Beans
 // Public accessor methods, constructor, equals, hashcode
@@ -25,6 +27,7 @@ public class HelloWorldConfiguration {
 	}
 
 	@Bean
+	@Primary
 	public Person person() {
 		return new Person("Kim", 20, new Address("Main Street", "Utrecht"));
 	}
@@ -39,12 +42,24 @@ public class HelloWorldConfiguration {
 		return new Person(name, age, address2); // name, age
 	}
 
+	@Bean
+	public Person person4Parameters(String name, int age, Address address) { // name, age, customAddress
+		return new Person(name, age, address); // name, age
+	}
+
+	@Bean
+	public Person person5Qualifier(String name, int age, @Qualifier("address2Qualifier") Address address) {
+		return new Person(name, age, address); // name, age
+	}
+
 	@Bean(name = "customAddress")
+	@Primary
 	public Address address() {
 		return new Address("Baker Street", "London");
 	}
 
 	@Bean(name = "address2")
+	@Qualifier("address2Qualifier")
 	public Address address2() {
 		return new Address("Street", "Suwon");
 	}
