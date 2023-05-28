33941b6d6163e79ac9d420986697bfe6ca5b9a24
Kim Ji Hun <gmldnr2222@naver.com>
2023-05-28 15:45:11 +0900

    feat: Spring Framework Bean Scopes - Prototype and Singleton
    - Spring Beans are defined to be used in a specific scope
    	- Singlton : One Object instance per Spring IoC Container
    	- Prototype : Possibly many object instances per Spring IoC container

---
+++ 01. Spring/study-spring-framework-02/src/main/java/com/jikim/study/examples/e1/BeanScopeLauncherApplication.java
@@ -0,0 +1,44 @@
+package com.jikim.study.examples.e1;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+class NormalClass {
+
+}
+
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+
+}
+
+@Configuration
+@ComponentScan
+public class BeanScopeLauncherApplication {
+
+	public static void main(String[] args) {
+		try (var context =
+				 new AnnotationConfigApplicationContext
+					 		(BeanScopeLauncherApplication.class)) {
+
+			// 기본적으로 Spring에서 생성되는 모든 Bean은 싱글톤.
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+		}
+	}
+}
