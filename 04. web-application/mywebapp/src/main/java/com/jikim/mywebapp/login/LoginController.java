a8f9e4bce459ed50488ae80398c730188beff62d
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-05 15:54:43 +0900

    feat: Displaying Login Login Credentials in a JSP using Model

--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/login/LoginController.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/login/LoginController.java
@@ -1,13 +1,24 @@
 package com.jikim.mywebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
 		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String gotoWelcomePage(@RequestParam String name,
+			@RequestParam String password, ModelMap model) {
+		model.put("name", name);
+		model.put("password", password);
+		return "welcome";
 	}
 }
---
+++ 04. web-application/mywebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <div>Welcome to Jikim Page</div>
+    <div>Your Name: ${name}</div>
+    <div>Your Password: ${password}</div>
+</body>
+</html>
