15d0e975728dbb65c74f5aaf040d852d2c36b510
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-05 16:59:08 +0900

    feat: Getting started with Todo Features - Creating Todo and TodoService

---
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
@@ -0,0 +1,23 @@
+package com.jikim.mywebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoService {
+
+	private static List<Todo> todos = new ArrayList<>();
+
+	static {
+		todos.add(new Todo(1, "jikim", "Learn AWS",
+			LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(1, "jikim", "Learn DevOps",
+			LocalDate.now().plusYears(2), false));
+		todos.add(new Todo(1, "jikim", "Learn Full Stack Development",
+			LocalDate.now().plusYears(3), false));
+	}
+
+	public List<Todo> findByUsername(String username) {
+		return todos;
+	}
+}
