8a74fcac19a5ea1de4d82cf3b3d8566f741e5207
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-05 17:39:47 +0900

    feat: Creating first version of List Todos Page

---
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoController.java
@@ -0,0 +1,25 @@
+package com.jikim.mywebapp.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TodoController {
+
+	private TodoService todoService;
+
+	public TodoController(TodoService todoService) {
+		this.todoService = todoService;
+	}
+
+	@RequestMapping(value = "list-todos", method = RequestMethod.GET)
+	public String listAllTodos(ModelMap model) {
+		List<Todo> todos = todoService.findByUsername("jikim");
+		model.addAttribute("todos", todos);
+		return "listTodos";
+	}
+}
--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
