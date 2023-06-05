812203dc151fd503ed44ab76feeae1d09472b585
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-05 15:11:51 +0900

    feat: Logging with Spring Boot

--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/login/LoginController.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.jikim.mywebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@RequestMapping("/login")
 	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
-		System.out.println("Request Param is " + name); // NOT RECOMMEND FOR PROD CODE
+		logger.debug("Request param is {}", name);
+		logger.info("I want this printed at info level");
+		logger.warn("I want this printed at warn level");
 		return "login";
 	}
 }
