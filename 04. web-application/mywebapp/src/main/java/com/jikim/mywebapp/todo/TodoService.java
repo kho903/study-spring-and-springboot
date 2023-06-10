f43c3dfc69157d1cb9c95ecfd57a67445e9ec5d9
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-10 16:26:40 +0900

    feat: Adding JSTL to Spring Boot Project and Showing Todos in a Table

--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
@@ -14,9 +14,9 @@ public class TodoService {
 	static {
 		todos.add(new Todo(1, "jikim", "Learn AWS",
 			LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(1, "jikim", "Learn DevOps",
+		todos.add(new Todo(2, "jikim", "Learn DevOps",
 			LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(1, "jikim", "Learn Full Stack Development",
+		todos.add(new Todo(3, "jikim", "Learn Full Stack Development",
 			LocalDate.now().plusYears(3), false));
 	}
 
--- 04. web-application/mywebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ 04. web-application/mywebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -3,8 +3,8 @@
     <title>Welcome Page</title>
 </head>
 <body>
-    <div>Welcome to Jikim Page</div>
-    <div>Your Name: ${name}</div>
+    <div>Welcome ${name}</div>
+    <hr>
     <div><a href="list-todos">Manage</a> your todos</div>
 </body>
 </html>
