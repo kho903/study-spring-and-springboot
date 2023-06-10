b433a14a53d83e2338fd4efad923590e378bb475
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-10 17:19:00 +0900

    feat: Enhancing TodoService to add the todo

--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
@@ -11,16 +11,23 @@ public class TodoService {
 
 	private static List<Todo> todos = new ArrayList<>();
 
+	private static int todosCount = 0;
+
 	static {
-		todos.add(new Todo(1, "jikim", "Learn AWS",
+		todos.add(new Todo(++todosCount, "jikim", "Learn AWS",
 			LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "jikim", "Learn DevOps",
+		todos.add(new Todo(++todosCount, "jikim", "Learn DevOps",
 			LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(3, "jikim", "Learn Full Stack Development",
+		todos.add(new Todo(++todosCount, "jikim", "Learn Full Stack Development",
 			LocalDate.now().plusYears(3), false));
 	}
 
 	public List<Todo> findByUsername(String username) {
 		return todos;
+	}
+
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+		todos.add(todo);
 	}
 }
