e0c1946173e1ca675d37df191d24cb58266a2953
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-11 17:02:57 +0900

    feat: Create TodoRepository and Connect List Todos page from H2 database

--- 04. web-application/mywebapp/src/main/resources/data.sql
+++ 04. web-application/mywebapp/src/main/resources/data.sql
@@ -8,7 +8,4 @@ insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values (10003, 'user', 'GET GCP Certified', current_date(), false);
 
 insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values (10004, 'user', 'GET AWS Certified', current_date(), false);
-
-insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values (10005, 'user', 'Learn DevOps', current_date(), false);
+values (10004, 'user', 'Learn DevOps', current_date(), false);
