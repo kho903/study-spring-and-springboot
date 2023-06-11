dd29d29cd640713509f28e919371e35217d42495
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-11 16:16:00 +0900

    refactor: Refactoring and Removing Hardcoding of User Id

--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/login/WelcomeController.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/login/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.jikim.mywebapp.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,13 @@ public class WelcomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String gotoWelcomePage(ModelMap model) {
-		model.put("name", "user");
+		model.put("name", getLoggedInUsername());
 		return "welcome";
+	}
+
+	private String getLoggedInUsername() {
+		Authentication authentication =
+			SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 }
--- 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
+++ 04. web-application/mywebapp/src/main/java/com/jikim/mywebapp/todo/TodoService.java
@@ -15,16 +15,18 @@ public class TodoService {
 	private static int todosCount = 0;
 
 	static {
-		todos.add(new Todo(++todosCount, "jikim", "Get AWS Certified",
+		todos.add(new Todo(++todosCount, "user", "Get AWS Certified",
 			LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(++todosCount, "jikim", "Learn DevOps",
+		todos.add(new Todo(++todosCount, "user", "Learn DevOps",
 			LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(++todosCount, "jikim", "Learn Full Stack Development",
+		todos.add(new Todo(++todosCount, "user", "Learn Full Stack Development",
 			LocalDate.now().plusYears(3), false));
 	}
 
 	public List<Todo> findByUsername(String username) {
-		return todos;
+		Predicate<? super Todo> predicate
+			= todo -> todo.getUsername().equalsIgnoreCase(username);
+		return todos.stream().filter(predicate).toList();
 	}
 
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
