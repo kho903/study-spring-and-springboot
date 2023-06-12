5c2c1e72ce251230f9d5f23c4e5e92ce30e786f3
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-12 15:22:54 +0900

    conf: start rest api project

---
+++ 05. REST API/rest-web-services/src/test/java/com/jikim/rest/webservices/RestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikim.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
