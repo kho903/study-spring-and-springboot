533698990ea08299f9d575bcab023f4ad5a4c540
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-13 15:24:17 +0900

    feat: Dynamic Filtering for REST API

--- 05. REST API/rest-web-services/src/main/java/com/jikim/rest/webservices/filtering/FilteringController.java
+++ 05. REST API/rest-web-services/src/main/java/com/jikim/rest/webservices/filtering/FilteringController.java
@@ -3,23 +3,37 @@ package com.jikim.rest.webservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
 	@GetMapping("/filtering")
-	public SomeBean filtering() {
-		return new SomeBean("value1", "value2", "value3");
+	public MappingJacksonValue filtering() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+		return getValueForFiltering(new MappingJacksonValue(someBean), "field1", "field3");
 	}
 
 	@GetMapping("/filtering-list")
-	public List<SomeBean> filteringList() {
-		return Arrays.asList(
+	public MappingJacksonValue filteringList() {
+		List<SomeBean> someBeans = Arrays.asList(
 			new SomeBean("value1", "value2", "value3"),
 			new SomeBean("value4", "value5", "value6"),
 			new SomeBean("value7", "value8", "value9")
 		);
+		return getValueForFiltering(new MappingJacksonValue(someBeans), "field2", "field3");
+	}
+
+	private MappingJacksonValue getValueForFiltering(MappingJacksonValue someBeans, String... field) {
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(field);
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		someBeans.setFilters(filters);
+		return someBeans;
 	}
 }
