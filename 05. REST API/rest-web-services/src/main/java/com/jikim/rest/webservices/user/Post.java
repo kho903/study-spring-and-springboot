336bd1ed61f79208bda574ac438652f4e33ee0de
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-13 17:54:10 +0900

    feat: Creating Post Entity with ManyToOne Relationship with User Entity

---
+++ 05. REST API/rest-web-services/src/main/java/com/jikim/rest/webservices/user/Post.java
@@ -0,0 +1,55 @@
+package com.jikim.rest.webservices.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private String id;
+
+	private String description;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
+	private User user;
+
+	public Post() {
+	}
+
+	public Post(String id, String description) {
+		this.id = id;
+		this.description = description;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Post{" +
+			"id='" + id + '\'' +
+			", description='" + description + '\'' +
+			'}';
+	}
+}
