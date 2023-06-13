a0f79dd163a919a7d8d5bb642bca544a2c4d46f5
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-13 13:09:18 +0900

    feat: Internationalization for REST API (i18n)

--- 05. REST API/rest-web-services/src/main/java/com/jikim/rest/webservices/helloworld/HelloWorldController.java
+++ 05. REST API/rest-web-services/src/main/java/com/jikim/rest/webservices/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.jikim.rest.webservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	private final MessageSource messageSource;
+
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -24,4 +34,19 @@ public class HelloWorldController {
 			String.format("Hello World, %s", name)
 		);
 	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		/*
+		 * en - English (Good Morning)
+		 * nl - Dutch (Goedemorgen)
+		 * fr - French (Bonjour)
+		 * de - Deutsch (Guten Morgen)
+		 * kr - Korean (좋은 아침)
+		 * */
+
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+	}
+
 }
