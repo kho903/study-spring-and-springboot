10f293dfa9b14e1ca6ceb1471f10e84e2e860e29
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-15 18:08:38 +0900

    feat: Spring Security - Storing User Credentials using JDBC

--- 06. Spring Security/spring-security/src/main/java/com/jikim/springsecurity/basic/BasicAuthSecurityConfiguration.java
+++ 06. Spring Security/spring-security/src/main/java/com/jikim/springsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,13 +1,19 @@
 package com.jikim.springsecurity.basic;
 
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.*;
+
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,19 +32,32 @@ public class BasicAuthSecurityConfiguration {
 		// http.formLogin();
 		http.httpBasic();
 		http.csrf().disable();
+		http.headers().frameOptions().sameOrigin();
 		return http.build();
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService() {
+	public UserDetailsService userDetailsService(DataSource dataSource) {
 		var user = User.withUsername("user")
 			.password("{noop}1234")
 			.roles("USER")
 			.build();
 		var admin = User.withUsername("admin")
 			.password("{noop}1234")
-			.roles("ADMIN")
+			.roles("ADMIN", "USER")
 			.build();
-		return new InMemoryUserDetailsManager(user, admin);
+
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
+		return jdbcUserDetailsManager;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setType(EmbeddedDatabaseType.H2)
+			.addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
+			.build();
 	}
 }
