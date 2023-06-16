cd2fa4192e1035379f738af3fd70439c94b79633
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-16 13:45:07 +0900

    feat: Setting up JWT Auth with Spring Security - 2 JWT Authentication using Spring Boot's OAuth2 Resource Server 1. Create Key Pair - We will use java.security.KeyPairGenerator - We can use openssl as well 2. Create RSA Key object using KeyPair - com.nimbusds.jose.jwk.RSAKey 3. Create JWKSource(JSON Web Key source) - Create JWKSet(a new JSON Web Key set) with the RSA Key - Create JWKSource using the JWKSet 4. Use RSA Public Key for Decoding - NimbusJwtDecoder.withPublicKey(rsaKey().toRSAPublicKey()).build() 5. Use JWKSource for Encoding - return new NimbusJwtEncoder(jwkSource()); - we will use this later in the JWT Resource

--- 06. Spring Security/spring-security/src/main/java/com/jikim/springsecurity/jwt/JwtSecurityConfiguration.java
+++ 06. Spring Security/spring-security/src/main/java/com/jikim/springsecurity/jwt/JwtSecurityConfiguration.java
@@ -2,6 +2,12 @@ package com.jikim.springsecurity.jwt;
 
 import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.*;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
@@ -11,12 +17,16 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration
 public class JwtSecurityConfiguration {
@@ -43,9 +53,9 @@ public class JwtSecurityConfiguration {
 	@Bean
 	public DataSource dataSource() {
 		return new EmbeddedDatabaseBuilder()
-			.setType(EmbeddedDatabaseType.H2)
-			.addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
-			.build();
+					.setType(EmbeddedDatabaseType.H2)
+					.addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
+					.build();
 	}
 
 	@Bean
@@ -53,8 +63,38 @@ public class JwtSecurityConfiguration {
 		return new BCryptPasswordEncoder();
 	}
 
-	// @Bean
-	// public JwtDecoder jwtDecoder() {
-	// 	return decoder();
-	// }
+	@Bean
+	public KeyPair keyPair() {
+		KeyPairGenerator keyPairGenerator = null;
+		try {
+			keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Bean
+	public RSAKey rsaKey(KeyPair keyPair) {
+		return new RSAKey
+					.Builder((RSAPublicKey)keyPair.getPublic())
+					.privateKey(keyPair.getPrivate())
+					.keyID(UUID.randomUUID().toString())
+					.build();
+	}
+
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+		var jwkSet = new JWKSet(rsaKey);
+
+		return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+		return NimbusJwtDecoder
+				.withPublicKey(rsaKey.toRSAPublicKey())
+				.build();
+	}
 }
