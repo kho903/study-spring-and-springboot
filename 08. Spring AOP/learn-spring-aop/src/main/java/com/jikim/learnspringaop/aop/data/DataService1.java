7e3b966e235ae5a9ee1ae256ce5211ebf30a07bc
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-17 12:48:31 +0900

    feat: Setting up Spring Components for Spring AOP

---
+++ 08. Spring AOP/learn-spring-aop/src/main/java/com/jikim/learnspringaop/aop/data/DataService1.java
@@ -0,0 +1,11 @@
+package com.jikim.learnspringaop.aop.data;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataService1 {
+
+	public int[] retrieveData() {
+		return new int[] {11, 22, 33, 44, 55};
+	}
+}
