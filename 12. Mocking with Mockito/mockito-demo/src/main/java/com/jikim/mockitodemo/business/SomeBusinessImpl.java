9ed1c08f273773dcf28d3e227231e6d872ed1efc
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-21 02:39:44 +0900

    feat: add business logic

---
+++ 12. Mocking with Mockito/mockito-demo/src/main/java/com/jikim/mockitodemo/business/SomeBusinessImpl.java
@@ -0,0 +1,19 @@
+package com.jikim.mockitodemo.business;
+
+public class SomeBusinessImpl {
+
+	private DataService dataService;
+
+	public int findTheGreatestFromAllData() {
+		int[] data = dataService.retrieveAllData();
+		int greatestValue = Integer.MIN_VALUE;
+		for (int value : data) {
+			if (value > greatestValue) greatestValue = value;
+		}
+		return greatestValue;
+	}
+}
+
+interface DataService {
+	int[] retrieveAllData();
+}
