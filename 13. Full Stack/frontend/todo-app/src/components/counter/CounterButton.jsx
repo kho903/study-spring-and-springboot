aeb45435dec497c473f7f720cf7ab3397cf9edac
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-26 19:58:03 +0900

    feat: Refactoring React Counter Component

--- 13. Full Stack/frontend/todo-app/src/components/counter/CounterButton.jsx
+++ 13. Full Stack/frontend/todo-app/src/components/counter/CounterButton.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
 import {PropTypes} from 'prop-types';
 
 export default function CounterButton({by, incrementMethod, decrementMethod}) {
-
-    // [0, f]
-    const [count, setCount] = useState(0);
-    
-    console.log(by);
-
-    function incrementCounterFunction() {
-        setCount(count + by);
-        incrementMethod(by);
-    }
-
-    function decrementCounterFunction() {
-        setCount(count - by);
-        decrementMethod(by);
-    }
-
     return (
         <div className="Counter">
             <div>
-                <button className="counterButton" onClick={incrementCounterFunction}>
+                <button className="counterButton" onClick={() => incrementMethod(by)}>
                     +{by}</button>
-                <button className="counterButton" onClick={decrementCounterFunction}>
+                <button className="counterButton" onClick={() => decrementMethod(by)}>
                     -{by}</button>
             </div>
         </div>
