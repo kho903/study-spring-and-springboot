5e32b657d0390aea15f272d83ce82c57ff4cf54a
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-26 15:10:05 +0900

    feat: Moving React State Up - Setting up Counter and Counter Button

--- 13. Full Stack/frontend/todo-app/src/App.js
+++ 13. Full Stack/frontend/todo-app/src/App.js
@@ -5,8 +5,6 @@ function App() {
   return (
     <div className="App">
       <Counter />
-      <Counter by={2} />
-      <Counter by={5} />
     </div>
   );
 }
--- 13. Full Stack/frontend/todo-app/src/components/counter/Counter.jsx
+++ 13. Full Stack/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
 import {PropTypes} from 'prop-types';
+import './Counter.css';
 
-export default function Counter({by}) {
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    function incrementCounterParentFunction(by) {
+        setCount(count + by);
+    }
+
+    return (
+        <>
+            <span className="count">{count}</span>
+            <CounterButton />
+            <CounterButton by={2} />
+            <CounterButton by={5} />
+        </>
+    )
+}
+
+function CounterButton({by}) {
 
     // [0, f]
     const [count, setCount] = useState(0);
@@ -29,10 +47,10 @@ export default function Counter({by}) {
     )
 }
 
-Counter.propTypes = {
+CounterButton.propTypes = {
     by: PropTypes.number
 }
 
-Counter.defaultProps = {
+CounterButton.defaultProps = {
     by: 1
 }
