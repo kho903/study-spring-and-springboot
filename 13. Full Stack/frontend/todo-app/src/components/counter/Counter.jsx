68533390da766770011c3a9e7097bf4fc2c9ead6
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-26 19:42:40 +0900

    feat: CounterButton as separate module

--- 13. Full Stack/frontend/todo-app/src/components/counter/Counter.jsx
+++ 13. Full Stack/frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import {PropTypes} from 'prop-types';
 import './Counter.css';
+import CounterButton from './CounterButton';
 
 export default function Counter() {
     const [count, setCount] = useState(0);
@@ -29,39 +29,3 @@ export default function Counter() {
     )
 }
 
-function CounterButton({by, incrementMethod, decrementMethod}) {
-
-    // [0, f]
-    const [count, setCount] = useState(0);
-    
-    console.log(by);
-
-    function incrementCounterFunction() {
-        setCount(count + by);
-        incrementMethod(by);
-    }
-
-    function decrementCounterFunction() {
-        setCount(count - by);
-        decrementMethod(by);
-    }
-
-    return (
-        <div className="Counter">
-            <div>
-                <button className="counterButton" onClick={incrementCounterFunction}>
-                    +{by}</button>
-                <button className="counterButton" onClick={decrementCounterFunction}>
-                    -{by}</button>
-            </div>
-        </div>
-    )
-}
-
-CounterButton.propTypes = {
-    by: PropTypes.number
-}
-
-CounterButton.defaultProps = {
-    by: 1
-}
