288b5fbd6578f2d6f736a13ee64451c297bb5d1d
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-27 12:53:09 +0900

    feat: Sharing React State with Multiple Components with Auth Context

--- 13. Full Stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ 13. Full Stack/frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -4,8 +4,6 @@ function WelcomeComponent() {
     
     const {username} = useParams()
 
-    console.log(username)
-
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
