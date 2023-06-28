f4a90426e76b5baa79f58bf38e172ed1a07a4acc
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-28 16:37:08 +0900

    feat: Using async and await to invoke Basic Auth API

--- 13. Full Stack/frontend/todo-app/src/components/todo/security/AuthContext.js
+++ 13. Full Stack/frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -12,6 +12,7 @@ export default function AuthProvider({ children }) {
     // 3. Put some state in the context
     const [isAuthenticated, setAuthenticated] = useState(false)
     const [username, setUsername] = useState(null)
+    const [token, setToken] = useState(null);
 
     // function login(username, password) {
         
@@ -26,31 +27,37 @@ export default function AuthProvider({ children }) {
     //     }
     // }
 
-    function login(username, password) {
+    async function login(username, password) {
         const baToken = 'Basic ' + window.btoa(username + ":" + password)
         
-        executeBasicAuthenticationService(baToken)
-            .then(response => console.log(response))
-            .catch(err => console.log(err))
+        try {
+            const response = await executeBasicAuthenticationService(baToken)
 
-        setAuthenticated(false)
-        // if (username === 'user' && password === 'password') {
-        //     setAuthenticated(true);
-        //     setUsername(username)
-        //     return true;
-        // } else {
-        //     setAuthenticated(false);
-        //     setUsername(null);
-        //     return false;
-        // }
+            if (response.status == 200) {
+                setAuthenticated(true)
+                setUsername(username)
+                setToken(baToken)
+                return true
+            } else {
+                setAuthenticated(false)
+                setUsername(null)
+                setToken(null)
+                return false
+            }
+        } catch(error) {
+            logout()
+            return false
+        }
     }
 
     function logout() {
-        setAuthenticated(false);
+        setAuthenticated(false)
+        setToken(null)
+        setUsername(null)
     }
 
     return (
-        <AuthContext.Provider value = { {isAuthenticated, login, logout, username} }>
+        <AuthContext.Provider value = { {isAuthenticated, login, logout, username, token} }>
             {children}
         </AuthContext.Provider>
     )
