90c33cf9ac208c9d842b58d723e4e49455d2a96b
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-28 16:53:54 +0900

    feat: Setting up Axios Interceptor to add Authorization Header

--- 13. Full Stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ 13. Full Stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -1,21 +1,17 @@
-import axios from "axios";
-
-const apiClient = axios.create(
-    {
-        baseURL: 'http://localhost:8080'
-    }
-)
+import { apiClient } from "./ApiClient";
 
 export const retrieveHelloWorldBean 
     = () => apiClient.get('/hello-world-bean')
 
 // header is present on the requested resource. => Authorization header
 export const retrieveHelloPathVariable
-    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`, {
-        headers: {
-            Authorization: token
-        }
-    })
+    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`
+    // , {
+    //     headers: {
+    //         Authorization: token
+    //     }
+    // }
+    )
 
 export const executeBasicAuthenticationService
     = (token) => apiClient.get(`/basicauth`, {
