e194e722aece2b1370433509c3edc0b23455ab4b
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-28 16:27:02 +0900

    feat: Calling Basic Authentication Service when Logging into React App

--- 13. Full Stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ 13. Full Stack/frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -17,3 +17,10 @@ export const retrieveHelloPathVariable
         }
     })
 
+export const executeBasicAuthenticationService
+    = (token) => apiClient.get(`/basicauth`, {
+        headers: {
+            Authorization: token
+        }
+    })
+
--- 13. Full Stack/frontend/todo-app/src/components/todo/security/AuthContext.js
+++ 13. Full Stack/frontend/todo-app/src/components/todo/security/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { executeBasicAuthenticationService } from "../api/HelloWorldApiService";
 
 // 1. Create a Context
 export const AuthContext = createContext()
@@ -12,17 +13,36 @@ export default function AuthProvider({ children }) {
     const [isAuthenticated, setAuthenticated] = useState(false)
     const [username, setUsername] = useState(null)
 
-    function login(username, password) {
+    // function login(username, password) {
         
-        if (username === 'user' && password === 'password') {
-            setAuthenticated(true);
-            setUsername(username)
-            return true;
-        } else {
-            setAuthenticated(false);
-            setUsername(null);
-            return false;
-        }
+    //     if (username === 'user' && password === 'password') {
+    //         setAuthenticated(true);
+    //         setUsername(username)
+    //         return true;
+    //     } else {
+    //         setAuthenticated(false);
+    //         setUsername(null);
+    //         return false;
+    //     }
+    // }
+
+    function login(username, password) {
+        const baToken = 'Basic ' + window.btoa(username + ":" + password)
+        
+        executeBasicAuthenticationService(baToken)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+
+        setAuthenticated(false)
+        // if (username === 'user' && password === 'password') {
+        //     setAuthenticated(true);
+        //     setUsername(username)
+        //     return true;
+        // } else {
+        //     setAuthenticated(false);
+        //     setUsername(null);
+        //     return false;
+        // }
     }
 
     function logout() {
