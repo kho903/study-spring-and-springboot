e87f78fcff87d92caeea623ca17586f1a377a14b
Kim Ji Hun <gmldnr2222@naver.com>
2023-06-28 15:34:14 +0900

    feat: Securing Spring Boot REST API with Spring Security

---
+++ 13. Full Stack/rest-api/src/main/java/com/jikim/restapi/basic/BasicAuthenticationSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.jikim.restapi.basic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthenticationSecurityConfiguration {
+
+	// Filter chain
+	// authenticate all requests
+	// basic authentication
+	// disabling csrf
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults())
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.csrf().disable()
+			.build();
+	}
+}
