f2f92f4750a919c901e26215db3a192d5f198700
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-27 21:53:00 +0900

    refactor: FP01 use System.out::println

--- 00. Java Functional Programming/functional-programming-with-java/src/main/java/jikim/FP01Functional.java
+++ 00. Java Functional Programming/functional-programming-with-java/src/main/java/jikim/FP01Functional.java
@@ -8,9 +8,9 @@ public class FP01Functional {
 		printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
 	}
 
-	private static void print(int number) {
+	/*private static void print(int number) {
 		System.out.println(number);
-	}
+	}*/
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
 		// [12, 9, 13, 4, 6, 2, 4, 12, 15]
@@ -20,8 +20,10 @@ public class FP01Functional {
 		// ....
 
 		// What to do?
+		// numbers.stream()
+		// 	.forEach(FP01Functional::print); // Method Reference
 		numbers.stream()
-			.forEach(FP01Functional::print); // Method Reference
+			.forEach(System.out::println);
 
 		// How to loop the numbers?
 		// for (int number : numbers) {
