9f7acc8db098b7bb9f101c5d3dc0506271e7369b
Kim Ji Hun <gmldnr2222@naver.com>
2023-07-27 22:06:22 +0900

    feat: FP01 printEvenNumber using filter, forEach

--- 00. Java Functional Programming/functional-programming-with-java/src/main/java/jikim/FP01Functional.java
+++ 00. Java Functional Programming/functional-programming-with-java/src/main/java/jikim/FP01Functional.java
@@ -5,7 +5,9 @@ import java.util.List;
 public class FP01Functional {
 
 	public static void main(String[] args) {
-		printAllNumbersInListFunctional(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		// printAllNumbersInListFunctional(numbers);
+		printEvenNumbersInListFunctional(numbers);
 	}
 
 	/*private static void print(int number) {
@@ -29,5 +31,16 @@ public class FP01Functional {
 		// for (int number : numbers) {
 		// 	System.out.println(number);
 		// }
+	}
+
+	private static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream()
+			// Filter - Only Allow Even Numbers
+			.filter(FP01Functional::isEven)
+			.forEach(System.out::println);
 	}
 }
--- 00. Java Functional Programming/functional-programming-with-java/src/main/java/jikim/FP01Structured.java
+++ 00. Java Functional Programming/functional-programming-with-java/src/main/java/jikim/FP01Structured.java
@@ -5,7 +5,9 @@ import java.util.List;
 public class FP01Structured {
 
 	public static void main(String[] args) {
-		printAllNumbersInListStructured(List.of(12, 9, 13, 4, 6, 2, 4, 12, 15));
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		// printAllNumbersInListStructured(numbers);
+		printEvenNumbersInListStructured(numbers);
 	}
 
 	private static void printAllNumbersInListStructured(List<Integer> numbers) {
@@ -14,4 +16,13 @@ public class FP01Structured {
 			System.out.println(number);
 		}
 	}
+
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+		// How to loop the numbers?
+		for (int number : numbers) {
+			if (number % 2 == 0)
+				System.out.println(number);
+		}
+	}
+
 }
